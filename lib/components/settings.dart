125c1f45a643ad880ebe582d2e93511d56195d6e
Akash <akash@alphaxb.com>
2024-04-10 17:29:59 +0600

    added fastlane

--- lib/components/settings.dart
+++ lib/components/settings.dart
@@ -413,7 +413,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   children: [
                     Text('App Name: Jar'),
                     SizedBox(height: 5.0),
-                    Text('Version: 1.0.7'),
+                    Text('Version: 1.0.8'),
                     SizedBox(height: 5.0),
                     Text('Developer: Niaz Mahmud Akash'),
                     SizedBox(height: 5.0),
